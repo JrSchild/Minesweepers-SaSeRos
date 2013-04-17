79cd67528234713d9df6ce6ef663879bfe993bbe
jalentheory <jhilum@hotmail.com>
2013-04-17 16:39:31 +0300

    Color Sensor Works!
    Testing and Black detection.

--- src/ColourSensor.java
+++ src/ColourSensor.java
@@ -2,16 +2,48 @@ import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 
 /**
- * @author Joram Ruitenschild
+ * @author Maimuna Syed :P
  * Abstraction for the light-sensor
  */
 public class ColourSensor {
 	private static final String name = "Colour Sensor";
 	private static final String port = "S4";
-	private static LightSensor lightSensor = new LightSensor(SensorPort.S4);
+	private static ColorHTSensor cmps = new ColorHTSensor(SensorPort.S4);
+	
+	public void colorSensor(){
+	  
+	 
+		LCD.clear();
+		LCD.drawString(colorSensorTest(), 1, 4);
+		LCD.drawInt(SensorPort.S4.readRawValue(),1,5);
+		 
+		    
+		if(blackDetected()){
+			LCD.drawString("BLACK", 1, 6); 
+		}
+		
+		LCD.refresh();
+		Thread.sleep(200);
+	 
+}
+
+/* public static String colorSensorTest(){
+	 if(SensorPort.S4.readRawValue() < 0){return "Color Sensor Fail";}
+	 return "Color Sensor Success";
+ }*/
+
+	public static boolean blackDetected(){
+		if (SensorPort.S4.readRawValue() > 600){
+			return true;
+		}
+		return false;
+	}
+
+
 	
 	public static Error checkForFaillure() {
-		Error error = new Error(name, port, true);
+		 if(SensorPort.S4.readRawValue() < 0){
+		Error error = new Error(name, port, true);}
 		return null;
 	}
 	
